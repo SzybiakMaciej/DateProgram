5d10ab12b61d9db99973b1eca8d7d63f93d8ee2d
Maciej Szybiak <szybiakmaciek83@gmail.com>
2019-06-06 23:39:11 +0200

    Add Project

---
+++ DateProgram.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DateProgram.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DateProgram.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34272a38-6e2e-4f4f-ba6b-47f35801ab16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DateProgram/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DateProgram
+{
+
+   public class DateConverter
+   {
+      private DateTime firstDate;
+      private DateTime secondDate;
+      private string resultConvertRange;
+
+      public DateConverter(string _firstDate, string _secondDate)
+      {
+         firstDate = DateTime.Parse(_firstDate, System.Globalization.CultureInfo.CreateSpecificCulture("de-DE"));
+         secondDate = DateTime.Parse(_secondDate, System.Globalization.CultureInfo.CreateSpecificCulture("de-DE"));
+      }
+
+      public string ConvertRange()
+      {
+         if (firstDate.Year == secondDate.Year)
+         {
+            if (firstDate.Month == secondDate.Month)
+            {
+               resultConvertRange = firstDate.ToString("dd") + "-" + secondDate.ToString("dd.MM.yyyy");
+            }
+            else if (firstDate.Month != secondDate.Month)
+            {
+               resultConvertRange = firstDate.ToString("dd.MM") + "-" + secondDate.ToString("dd.MM.yyyy");
+            }
+         }
+         else
+         {
+            resultConvertRange = firstDate.ToString("dd.MM.yyyy") + "-" + secondDate.ToString("dd.MM.yyyy");
+         }
+         return resultConvertRange;
+      }
+
+      public void ShowRange()
+      {
+         Console.WriteLine(resultConvertRange);
+      }
+
+   }
+
+   public class Program
+   {
+
+
+      public static void Main(string[] args)
+      {
+         string dateOne = Console.ReadLine();
+         string dateTwo = Console.ReadLine();
+
+         DateConverter date = new DateConverter(dateOne, dateTwo);
+
+         date.ConvertRange();
+         date.ShowRange();
+         
+
+         Console.ReadKey();
+      }
+   }
+}
